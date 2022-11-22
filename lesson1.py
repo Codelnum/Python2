d5d76aac9fa2feb1b0e50945e2b14a3c2e08e145
Codelnum <mvpugachev@mail.ru>
2022-11-22 21:42:30 +0300

    lesson1

--- lesson1.py
+++ lesson1.py
@@ -8,10 +8,10 @@
 
 
 #2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них
-
-list_numbers = []
+max= float('-inf')
 i=1
 for i in range(1,6):
     num = int(input(f"input num {i}: "))
-    list_numbers.append(num)
-print(max(list_numbers))
+    if num > max:
+        max=num
+print(max)
