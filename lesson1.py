bdeca424e32500f7502bfb10a330ac4c343730da
Codelnum <mvpugachev@mail.ru>
2022-11-22 21:23:47 +0300

    lesson_1

--- lesson1.py
+++ lesson1.py
@@ -1,7 +1,17 @@
-#Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
-a = int(input('input a:  '))
-b = int(input('input b:  '))
-if a*a == b or b*b==a :
-    print("yes")
-else:
-    print("no")
+#1. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+# a = int(input('input a:  '))
+# b = int(input('input b:  '))
+# if a*a == b or b*b==a :
+#     print("yes")
+# else:
+#     print("no")
+
+
+#2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них
+
+list_numbers = []
+i=1
+for i in range(1,6):
+    num = int(input(f"input num {i}: "))
+    list_numbers.append(num)
+print(max(list_numbers))
