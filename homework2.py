09b2fb9f8e71829d8b0a19d63b8c17eac3aa1d25
Codelnum <mvpugachev@mail.ru>
2022-11-27 15:34:28 +0300

    homework2

--- homework2.py
+++ homework2.py
@@ -2,21 +2,21 @@
 
 num_list = list(input('enter nums:  ').replace(',', '').replace('.',''))
 sum = 0
-i=0
+i = 0
 while i < len(num_list):
     #почему не работает for i in num_list( list indices must be integers or slices, not str)?
     sum = sum + int(num_list[i])
-    i+=1
+    i = i + 1
 print(sum)
 
 
 # 2 Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 
 num = int(input('enter num:  '))  #3
-mult_list=[]
-mult=1
-for i in range(1,num+1):
-    mult = mult*i
+mult_list = []
+mult = 1
+for i in range(1, num + 1):
+    mult = mult * i
     mult_list.append(mult)
 print(mult_list)
 
@@ -27,8 +27,8 @@ print(mult_list)
 n = int(input('enter n:  '))
 
 list = []
-for i in range(1, n+1):
-    list.append(round(((1 + 1 / i)** i), 2))
+for i in range(1, n + 1):
+    list.append(round(((1 + 1 / i)**i ), 2))
 print(list)
 sum = 0
 for i in range(0, len(list)):
@@ -38,10 +38,10 @@ print(sum)
 
 # 4 Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции вводятся с клавиатуры.
 n = int(input('enter n:  '))
-list =[]
+list = []
 for i in range(-n, n + 1):
     list.append(-n)
-    n=n-1
+    n = n - 1
 print(list)
 str = input('enter value1 value2 with space:  ')
 sum_value = list[int(str[0])] + list[int(str[2])]
@@ -51,12 +51,12 @@ print(f'value sum = {sum_value}')
 # Из библиотеки random использовать можно только randint
 
 import random
-main_list =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-mixed_list =[]
+main_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+mixed_list = []
 print(f'main list: {main_list}')
 
-while len(mixed_list) <len(main_list):
-    rnd = random.randint(1,10)
+while len(mixed_list) < len(main_list):
+    rnd = random.randint(1, 10)
     if rnd in mixed_list:
       pass  
     else:
