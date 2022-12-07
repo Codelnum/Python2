d8d982e63fd7ea79429081191dd55c83fe077104
Codelnum <mvpugachev@mail.ru>
2022-12-08 00:31:49 +0300

    homework4

--- homework4.py
+++ homework4.py
@@ -1,19 +1,20 @@
 # 1 Вычислить число π c заданной точностью d
 # *Пример:* 
 # - при $d = 0.001, π = 3.141.$ 
+
 # d = float(input('enter d: '))
-# n=2
-# m=3
-# n2=4
-# pi=1
-# count=0
-# while count < 1/d:
-#     pi = pi* (n/m) * (n2/m)
-#     n=n+2
-#     m=m+2
-#     n2=n2+2
-#     count+=1
-# pi = pi*4
+# n = 2
+# m = 3
+# n2 = 4
+# pi = 1
+# count = 0
+# while count < 1 / d:
+#     pi = pi * (n / m) * (n2 / m)
+#     n = n + 2
+#     m = m + 2
+#     n2 = n2 + 2
+#     count + = 1
+# pi = pi * 4
 # print(pi)
 
 
@@ -22,14 +23,15 @@
 # *Пример*
 # - при N=236     ->        [2, 2, 59]
 # n = int(input('enter N:  '))
+
 # n_list = []
-# x=2
-# while n//x!=0:
-#     if n%x==0:
+# x = 2
+# while n // x != 0:
+#     if n % x == 0:
 #         n_list.append(x)
-#         n=n//x
+#         n = n // x
 #     else:
-#         x=x+1
+#         x = x + 1
 
 # print(n_list)
 
@@ -41,28 +43,30 @@
 # input_list = (input('enter numbers with space: ').split())
 # result_list = []
 # for i in input_list:
-#     if input_list.count(i)<2:
+#     if input_list.count(i) < 2:
 #         result_list.append(i)
 # print(result_list)
+
+
 
 # 4 Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # *Пример:* 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
 # import random
-# k = random.randint(1,5)
+# k = random.randint(1, 5)
 
 # def a(x):
-#     a = random.randint(2, 10*x)
+#     a = random.randint(2, 10 * x)
 #     return a
 
 # result_list = []
 
 # def mult(x):
-#     mult_string =(f'{a(x)}*x**{x}')
-#     while x>1:
-#         mult_string =mult_string+ ' + ' +(f'{a(x)}*x**{x-1}')
-#         x-=1 
+#     mult_string =(f'{a(x)} * x **{x}')
+#     while x > 1:
+#         mult_string =mult_string + ' + ' + (f'{a(x)} * x ** {x - 1}')
+#         x - = 1 
 #     return mult_string
 
 # file = open('hw4(1).txt', 'w')
@@ -89,10 +93,10 @@ lst1.reverse()
 print(lst1)
 
 
-# file = open('hw4(2).txt', 'r')
-# str2 = file.read()
-# file.close()
-# str2= str2.replace('*','').replace('+','').replace('= 0','')
-# lst2=list(str2.split())
-# lst2.reverse()
-# print(lst2)
+file = open('hw4(2).txt', 'r')
+str2 = file.read()
+file.close()
+str2= str2.replace('*','').replace('+','').replace('= 0','')
+lst2=list(str2.split())
+lst2.reverse()
+print(lst2)
